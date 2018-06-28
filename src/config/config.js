0c271e4c4ba084e3a96ceae69102f4b4aa8fade3
Lamis El-Zobeidy <lamis.zobeidy@gmail.com>
2018-06-28 02:45:07 +0200

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+
+  // Initialize Firebase
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyBmkebAM2YESQbvXP74WK0V5QwKEUZ-5QY",
+    authDomain: "reactnotes-1f1c1.firebaseapp.com",
+    databaseURL: "https://reactnotes-1f1c1.firebaseio.com",
+    projectId: "reactnotes-1f1c1",
+    storageBucket: "reactnotes-1f1c1.appspot.com",
+    messagingSenderId: "525283775024"
+  };
